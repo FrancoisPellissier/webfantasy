b1abca974ac56cfe662d43eca3a0901bcfaff390
FrancoisPellissier <f_pellissier@hotmail.com>
2016-07-02 14:16:10 +0200

    Add Error module

---
+++ modules/Error/ErrorController.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace modules\Error;
+
+class ErrorController extends \library\BaseController {
+	
+	public function error_404() {
+		$this->titre_page = 'Page introuvable';
+		$this->view->addHeader('HTTP/1.0 404 Not Found');
+		$this->makeView();
+	}
+
+	public function maintenance() {
+		$this->titre_page = 'Maintenance';
+		$this->makeView();
+	}
+}
---
+++ modules/Error/view/maintenance.tpl.php
@@ -0,0 +1,3 @@
+<div class="alert alert-danger" role="alert">
+	<p>Le site est actuellement en maintenance, il sera de nouveau en ligne sous peu.</p>
+</div>
