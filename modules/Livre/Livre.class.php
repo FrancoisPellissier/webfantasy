856a7e7a103c4e176eed8209e299aa5341f5cf6d
FrancoisPellissier <f_pellissier@hotmail.com>
2016-07-20 13:45:05 +0200

    Livre: base and sidebar

--- modules/Livre/Livre.class.php
+++ modules/Livre/Livre.class.php
@@ -20,6 +20,15 @@ class Livre extends \library\BaseModel {
         );
     }
 
+    public function getCycle($auteurid = 0) {
+        // On récupère le cycle associé
+        $cycle = new \modules\Cycle\Cycle();
+        $cycle->exists($this->infos['cycleid']);
+        $cycle->getLivres();
+
+        $this->infos['cycle'] = $cycle->infos;
+    }
+
     public function getEditions() {
         $editions = array();
         if($this->exists) {
@@ -33,5 +42,63 @@ class Livre extends \library\BaseModel {
             $editions = $this->getResults($sql);
         }
         return $editions;
+    }
+
+    // Génération des sections de la sidebar
+    public function sidebar($section, $auteurid = 0, $cycleid = 0) {
+        $items = array();
+
+        if($section == 'main') {
+            $baselink = 'livre/'.$auteurid.'/'.$this->infos['livreid'].'/'.$this->slug($this->infos['titre']);
+
+            $items[] = array(
+                'href' => $baselink,
+                'value' => 'Fiche'
+            );
+
+            $items[] = array(
+                'href' => $baselink.'/edition',
+                'value' => 'Editions'
+            );
+
+            /* **** TO DO
+            ** A paramétrer si une galerie existe
+            */
+            $items[] = array(
+                'href' => $baselink.'',
+                'value' => 'Galerie'
+            );
+
+            /* **** TO DO
+            ** A paramétrer si un topic d'avis existe (ou nouveau système s'il y a)
+            */
+            $items[] = array(
+                'href' => $baselink.'',
+                'value' => 'Avis'
+            );
+
+            // Affichage de la liste des pages de 1er niveau
+            if(isset($this->infos['pages'])) {
+                foreach($this->infos['pages'] AS $page) {
+                    $items[] = array(
+                        'href' => $baselink.'/'.$page->infos['pageid'].'/'.$this->slug($page->infos['titre']),
+                        'value' => $page->infos['titre']
+                    );
+                }
+            }
+        }
+        else if($section == 'livre') {
+            $baselink = 'livre/'.$auteurid.'/';
+            
+            if(isset($this->infos['cycle']['livre'])) {
+                foreach($this->infos['cycle']['livre'] AS $livre) {
+                    $items[] = array(
+                        'href' => $baselink.$livre->infos['livreid'].'/'.$this->slug($livre->infos['titre']),
+                        'value' => $livre->infos['titre']
+                    );
+                }
+            }
+        }
+        return $items;
     }
 }
--- modules/Livre/LivreController.class.php
+++ modules/Livre/LivreController.class.php
@@ -2,19 +2,46 @@
 namespace modules\Livre;
 
 class LivreController extends \library\BaseController {
-	public function showLivre() {
+    public function getCommon() {
+        $model = $this->exists();
+        $auteurid = intval($this->request->getData('auteurid'));
+        $model->getCycle($auteurid);
 
-	}
+        // Ajout des éléments dans la Sidebar
+        $this->view->sidebarAdd('title', $model->infos['titre']);
+        $this->view->sidebarAdd('link', $model->sidebar('main', $auteurid));
+        
+        /* ** TO DO **
+        ** A paramétrer en fonction cycle oui / non
+        ** Si non, afficher "cycle du même auteur" ou "du même auteur"
+        ** ** Fin du TO **
+        */
+        $this->view->sidebarAdd('title', 'Dans le même cycle');
+        $this->view->sidebarAdd('link', $model->sidebar('livre', $auteurid));
 
-	public function showEditions() {
+        // Génération du nom de la page
+        $this->view->setTitle($model->infos['titre']);
 
-	}
+        return array($model, $view);
+    }
 
-	public function showPages() {
+    public function showLivre() {
+        $common = $this->getCommon();
+        $model = $common[0];
+        $view = $common[1];
 
-	}
+        $this->makeView();
+    }
 
-	public function showPage() {
-		
-	}
+    public function showEditions() {
+
+    }
+
+    public function showPages() {
+
+    }
+
+    public function showPage() {
+        
+    }
 }
