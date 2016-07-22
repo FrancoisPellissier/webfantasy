f89f2226699ef9d37ede7577f6d2508144217457
FrancoisPellissier <f_pellissier@hotmail.com>
2016-07-22 13:21:58 +0200

    Del defaut ariane

--- assets/layouts/view.php
+++ assets/layouts/view.php
@@ -2,19 +2,8 @@
     <div id="contenu-right">
         <div id="arbo">
             <?php
-            echo "\n\t".'<a href="'.WWW_ROOT.'">Accueil</a>';
-
-            // Données tests temporaires
-            if(empty($fil_ariane)) {
-                $fil_ariane = array(
-                    array('url' => '', 'title' => 'Terry Goodkind'),
-                    array('url' => '', 'title' => 'L\'Épée de Vérité'),
-                    array('url' => '', 'title' => 'La Première Leçon du Sorcier'),
-                    array('url' => '', 'title' => 'Éditions')
-                    );
-                }
-
             // Affichage du fil d'Ariane
+            echo "\n\t".'<a href="'.WWW_ROOT.'">Accueil</a>';
             foreach($fil_ariane AS $ariane) {
                 echo ' > <a href="'.$ariane['url'].'">'.$ariane['title'].'</a>';
             }
