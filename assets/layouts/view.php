61e363b53c992e5758380019801fd7a293d6b5c7
FrancoisPellissier <f_pellissier@hotmail.com>
2016-07-22 12:55:40 +0200

    Handle fil ariane in layout

--- assets/layouts/view.php
+++ assets/layouts/view.php
@@ -1,7 +1,23 @@
 <div id="right">
     <div id="contenu-right">
         <div id="arbo">
-            <strong>Vous êtes ici :</strong> <i>Accueil > Bienvenue sur Votre Société.com !</i>
+            <?php
+            echo "\n\t".'<a href="'.WWW_ROOT.'">Accueil</a>';
+
+            // Données tests temporaires
+            if(empty($fil_ariane)) {
+                $fil_ariane = array(
+                    array('url' => '', 'title' => 'Terry Goodkind'),
+                    array('url' => '', 'title' => 'L\'Épée de Vérité'),
+                    array('url' => '', 'title' => 'La Première Leçon du Sorcier'),
+                    array('url' => '', 'title' => 'Éditions')
+                    );
+
+                foreach($fil_ariane AS $ariane) {
+                    echo ' > <a href="'.$ariane['url'].'">'.$ariane['title'].'</a>';
+                }
+            }
+            ?>
         </div>
 <?php
 // Contenu du site
