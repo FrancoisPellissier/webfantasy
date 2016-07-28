dd2a2662e7d5ce866e299eee6047f5437de6d31a
FrancoisPellissier <f_pellissier@hotmail.com>
2016-07-28 13:01:11 +0200

    Cycle: handle is_cycle in form

--- modules/Cycle/view/add.tpl.php
+++ modules/Cycle/view/add.tpl.php
@@ -1,6 +1,7 @@
 <form method="post">
     <p>Titre VO <input type="text" name="data[titre_vo]" value="" /></p>
     <p>Titre VF <input type="text" name="data[titre_vf]" value="" /></p>
+     <p>Cycle : <input type="radio" id="is_cycle_oui" name="data[is_cycle]" value="1" /> <label for="is_cycle_oui">Oui</label> / <input type="radio" id="is_cycle_non" name="data[is_cycle]" value="0" /> <label for="is_cycle_non">Non</label></p>
 
     <p>Description :</p>
     <textarea name="data[description]" cols="70" rows="20" ></textarea>
--- modules/Cycle/view/edit.tpl.php
+++ modules/Cycle/view/edit.tpl.php
@@ -1,6 +1,7 @@
 <form method="post">
     <p>Titre VO <input type="text" name="data[titre_vo]" value="<?php echo $model->infos['titre_vo']; ?>" /></p>
     <p>Titre VF <input type="text" name="data[titre_vf]" value="<?php echo $model->infos['titre_vf']; ?>" /></p>
+    <p>Cycle : <input type="radio" id="is_cycle_oui" name="data[is_cycle]" value="1"<?php echo ($model->infos['is_cycle'] ? ' checked' : ''); ?> /> <label for="is_cycle_oui">Oui</label> / <input type="radio" id="is_cycle_non" name="data[is_cycle]" value="0"<?php echo (!$model->infos['is_cycle'] ? ' checked' : ''); ?> /> <label for="is_cycle_non">Non</label></p>
 
     <p>Description :</p>
     <textarea name="data[description]" cols="70" rows="20" ><?php echo $model->infos['description']; ?></textarea>
