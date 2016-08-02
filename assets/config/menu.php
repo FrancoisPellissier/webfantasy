64f26f3dfd63400fd589be8c670995245d5ad8c6
FrancoisPellissier <f_pellissier@hotmail.com>
2016-08-02 13:52:40 +0200

    Config menu by domaine

---
+++ assets/config/menu.php
@@ -0,0 +1,45 @@
+<?php
+$menuItems = array();
+
+// WebFantasy
+$items = array();
+$items[] = array('url' => '', 'title' => 'Accueil');
+$items[] = array('url' => '', 'title' => 'Le Concept');
+$items[] = array('url' => '', 'title' => 'Nos sites');
+$items[] = array('url' => '', 'title' => 'Les auteurs');
+$items[] = array('url' => '', 'title' => 'Les livres');
+$items[] = array('url' => '', 'title' => 'Concours');
+$items[] = array('url' => '', 'title' => 'Forum');
+$menuItems[0] = $items;
+
+// Terry Goodkind
+$items = array();
+$items[] = array('url' => '', 'title' => 'Accueil');
+$items[] = array('url' => 'auteur/1/terry-goodkind', 'title' => 'L\'auteur');
+$items[] = array('url' => 'auteur/1/terry-goodkind/livres', 'title' => 'Les livres');
+$items[] = array('url' => '', 'title' => 'Interviews');
+$items[] = array('url' => '', 'title' => 'Galerie');
+$items[] = array('url' => '', 'title' => 'Wiki');
+$items[] = array('url' => 'forum', 'title' => 'Forum');
+$menuItems[1] = $items;
+
+// Patrick Rothfuss
+$items = array();
+$items[] = array('url' => '', 'title' => 'Accueil');
+$items[] = array('url' => 'auteur/2/patrick-rothfuss', 'title' => 'L\'auteur');
+$items[] = array('url' => 'auteur/2/patrick-rothfuss/livres', 'title' => 'Les livres');
+$items[] = array('url' => '', 'title' => 'Interviews');
+$items[] = array('url' => '', 'title' => 'Galerie');
+$items[] = array('url' => '', 'title' => 'Wiki');
+$items[] = array('url' => 'forum', 'title' => 'Forum');
+$menuItems[2] = $items;
+
+// Brandon Sanderson
+$items = array();
+$items[] = array('url' => '', 'title' => 'Accueil');
+$items[] = array('url' => 'auteur/3/brandon-sanderson', 'title' => 'L\'auteur');
+$items[] = array('url' => 'auteur/3/brandon-sanderson/livres', 'title' => 'Les livres');
+$items[] = array('url' => '', 'title' => 'Interviews');
+$items[] = array('url' => '', 'title' => 'Galerie');
+$items[] = array('url' => 'forum', 'title' => 'Forum');
+$menuItems[3] = $items;
--- assets/layouts/menu.php
+++ assets/layouts/menu.php
@@ -1,17 +1,16 @@
 <?php
-$auteur = array(
-	'1' => 'auteur/1/terry-goodkind',
-	'2' => 'auteur/2/patrick-rothfuss',
-	'3' => 'auteur/3/brandon-sanderson'
-	);
+include('assets/config/menu.php');
+
+
 ?>
 <div id="site">
     <div id="header"></div>
     <div id="sous-header">
         <div id="mh">
-            <a title="Accueil" href="">Accueil</a>
-            <a title="L'auteur" href="<?php echo $auteur[$domaine]; ?>">L'auteur</a>
-            <a title="Les livres" href="<?php echo $auteur[$domaine]; ?>/livre">Les livres</a>
-            <a title="Accéder au forum" href="forum">Forum</a>
+            <?php
+            foreach($menuItems[$domaine] AS $menuItem) {
+                echo "\n\t\t\t".'<a href="'.$menuItem['url'].'">'.$menuItem['title'].'</a>';
+            }
+            ?>
         </div>
     </div>
