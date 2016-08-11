b34b0c6849100d6a16b74b1a664ba162d46d4cf2
FrancoisPellissier <f_pellissier@hotmail.com>
2016-08-11 13:29:24 +0200

    Change image columns name

--- misc/migration_galerie.sql
+++ misc/migration_galerie.sql
@@ -4,8 +4,8 @@ DROP TABLE IF EXISTS site_image;
 CREATE TABLE site_image (
 	imageid INT NOT NULL AUTO_INCREMENT,
 	categoryid INT NOT NULL DEFAULT 0,
-	titre VARCHAR(255) NOT NULL DEFAULT '',
-	description TEXT NULL DEFAULT NULL,
+	title VARCHAR(255) NOT NULL DEFAULT '',
+	commentaire TEXT NULL DEFAULT NULL,
 	folder VARCHAR(255) NOT NULL DEFAULT '',
 	filename VARCHAR(255) NOT NULL DEFAULT '',
 	created_at DATETIME NULL DEFAULT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE site_image (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Récupération des images liées à l'une des catégories migrées
-INSERT INTO site_image (imageid, titre, description, folder, filename, created_at, updated_at)
+INSERT INTO site_image (imageid, title, commentaire, folder, filename, created_at, updated_at)
 	SELECT
 		i.id,
 		i.`name`,
--- modules/Image/Image.class.php
+++ modules/Image/Image.class.php
@@ -14,8 +14,8 @@ class Image extends \library\BaseModel {
         $this->schema = array(
             'imageid' => array('fieldtype' => 'INT', 'required' => false, 'default' => '', 'publicname' => 'ID de l image'),
             'categoryid' => array('fieldtype' => 'INT', 'required' => false, 'default' => '0', 'publicname' => 'Categorie principale associée'),
-            'titre' => array('fieldtype' => 'VARCHAR', 'required' => false, 'default' => '', 'publicname' => 'Titre de l image'),
-            'description' => array('fieldtype' => 'TEXT', 'required' => false, 'default' => '', 'publicname' => 'Description de l image'),
+            'title' => array('fieldtype' => 'VARCHAR', 'required' => false, 'default' => '', 'publicname' => 'Titre de l image'),
+            'commentaire' => array('fieldtype' => 'TEXT', 'required' => false, 'default' => '', 'publicname' => 'Description de l image'),
             'folder' => array('fieldtype' => 'VARCHAR', 'required' => false, 'default' => '', 'publicname' => 'Dossier de stockage'),
             'filename' => array('fieldtype' => 'VARCHAR', 'required' => false, 'default' => '', 'publicname' => 'Nom du fichier')
         );
