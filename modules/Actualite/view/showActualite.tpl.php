99580dc39870d570ddeb72240c2c3fc3963de7a7
FrancoisPellissier <f_pellissier@hotmail.com>
2016-08-18 12:16:29 +0200

    Link to newsletter in actualite

--- modules/Actualite/view/showActualite.tpl.php
+++ modules/Actualite/view/showActualite.tpl.php
@@ -5,6 +5,9 @@ echo "\n\t".'<p>Par <em>'.$model->infos['poster'].'</em> le <em>'.$model->infos[
     
 echo "\n\t".parse_message($model->infos['message'], 0);
 
+
+echo "\n\t".'<p>Vous ne voulez manquer aucune nouveautée ? <a href="newsletter">Inscrivez-vous à la newsletter</a></p>';
+
 if(!empty($model->infos['comments'])) {
     foreach($model->infos['comments'] AS $comment) {
         echo "\n\t".'<h3>Par <em>'.$comment->infos['poster'].'</em> le <em>'.$comment->infos['posted'].'</em></h3>';
