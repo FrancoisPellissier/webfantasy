ea6b83ca69d69e29f298e4b93a025798be436531
FrancoisPellissier <f_pellissier@hotmail.com>
2016-08-22 13:45:24 +0200

    Display no actualite

--- modules/Actualite/view/showArchive.tpl.php
+++ modules/Actualite/view/showArchive.tpl.php
@@ -1,5 +1,6 @@
 <?php
 echo "\n\t".'<p>Acrhives : <select name="periode" id="periode" onChange="choosePeriode()">';
+echo "\n\t\t".'<option value="">Choix de la période</option>';
 
 $list_mois = array(1 => 'janvier', 2 => 'février', 3 => 'mars', 4 => 'avril', 5 => 'mai', 6 => 'juin', 7 => 'juillet', 8 => 'août', 9 => 'septembre', 10 => 'octobre', 11 => 'novembre', 12 => 'décembre');
 
@@ -19,14 +20,18 @@ foreach($list_months AS $month) {
 echo "\n\t".'</select></p>';
 
 
+if(empty($actualites)) {
+    echo "\n\t".'<p>Aucune actualité pour cette période</p>';
+}
+else {
+    foreach($actualites AS $actualite) {
+        echo "\n\t".'<h1><a href="'.$actualite->getSlug().'">'.$actualite->infos['subject'].'</a></h1>';
 
-foreach($actualites AS $actualite) {
-    echo "\n\t".'<h1><a href="'.$actualite->getSlug().'">'.$actualite->infos['subject'].'</a></h1>';
-
-    echo "\n\t".'<p>Par <em>'.$actualite->infos['poster'].'</em> le <em>'.\library\Timestamp::formatDateTime($actualite->infos['posted'], 'j mois Y', 'H:i:s').'</em></p>';
-    
-    echo "\n\t".parse_message(substr($actualite->infos['message'], 0, strpos(nl2br($actualite->infos['message']), '<br />')), 0);
-    
-    echo "\n\t".'<p><a href="'.$actualite->getSlug().'">Lire la suite</a></p>';
+        echo "\n\t".'<p>Par <em>'.$actualite->infos['poster'].'</em> le <em>'.\library\Timestamp::formatDateTime($actualite->infos['posted'], 'j mois Y', 'H:i:s').'</em></p>';
+        
+        echo "\n\t".parse_message(substr($actualite->infos['message'], 0, strpos(nl2br($actualite->infos['message']), '<br />')), 0);
+        
+        echo "\n\t".'<p><a href="'.$actualite->getSlug().'">Lire la suite</a></p>';
+    }
 }
 ?>
