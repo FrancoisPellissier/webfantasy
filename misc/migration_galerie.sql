20f6c34893a14a1656a0f8fcf73abdd394bb3b11
FrancoisPellissier <f_pellissier@hotmail.com>
2016-08-23 08:57:28 +0200

    Reclass image form Pays to Cover

--- misc/migration_galerie.sql
+++ misc/migration_galerie.sql
@@ -1,3 +1,15 @@
+
+
+
+-- Créer la fichie livre manquante (Sixth at the Dusk) avant la migration pour avoir la catégorie manquante ligne 114.
+
+
+
+
+
+
+
+
 /************************** Images ****************************/
 -- Création de la structure
 DROP TABLE IF EXISTS site_image;
@@ -38,6 +50,7 @@ CREATE TABLE site_image_category (
 	PRIMARY KEY(imageid, categoryid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- Insertion des liens existants
 INSERT INTO site_image_category (
 	imageid,
 	categoryid)
@@ -53,6 +66,53 @@ INSERT INTO site_image_category (
 		i.id,
 		ic.category_id;
 
+-- Forçage de certains catégories
+UPDATE site_image SET categoryid = 403 WHERE imageid = 292;
+UPDATE site_image SET categoryid = 402 WHERE imageid = 293;
+UPDATE site_image SET categoryid = 402 WHERE imageid = 294;
+UPDATE site_image SET categoryid = 401 WHERE imageid = 295;
+UPDATE site_image SET categoryid = 400 WHERE imageid = 296;
+UPDATE site_image SET categoryid = 400 WHERE imageid = 297;
+UPDATE site_image SET categoryid = 399 WHERE imageid = 298;
+UPDATE site_image SET categoryid = 399 WHERE imageid = 299;
+UPDATE site_image SET categoryid = 398 WHERE imageid = 300;
+UPDATE site_image SET categoryid = 398 WHERE imageid = 301;
+UPDATE site_image SET categoryid = 406 WHERE imageid = 302;
+UPDATE site_image SET categoryid = 406 WHERE imageid = 303;
+UPDATE site_image SET categoryid = 397 WHERE imageid = 304;
+UPDATE site_image SET categoryid = 397 WHERE imageid = 305;
+UPDATE site_image SET categoryid = 396 WHERE imageid = 306;
+UPDATE site_image SET categoryid = 396 WHERE imageid = 307;
+UPDATE site_image SET categoryid = 395 WHERE imageid = 308;
+UPDATE site_image SET categoryid = 405 WHERE imageid = 309;
+UPDATE site_image SET categoryid = 403 WHERE imageid = 1306;
+UPDATE site_image SET categoryid = 402 WHERE imageid = 1307;
+UPDATE site_image SET categoryid = 402 WHERE imageid = 1308;
+UPDATE site_image SET categoryid = 402 WHERE imageid = 1309;
+UPDATE site_image SET categoryid = 401 WHERE imageid = 1310;
+UPDATE site_image SET categoryid = 401 WHERE imageid = 1311;
+UPDATE site_image SET categoryid = 400 WHERE imageid = 1312;
+UPDATE site_image SET categoryid = 400 WHERE imageid = 1313;
+UPDATE site_image SET categoryid = 400 WHERE imageid = 1314;
+UPDATE site_image SET categoryid = 399 WHERE imageid = 1315;
+UPDATE site_image SET categoryid = 399 WHERE imageid = 1316;
+UPDATE site_image SET categoryid = 404 WHERE imageid = 1662;
+UPDATE site_image SET categoryid = 404 WHERE imageid = 1752;
+UPDATE site_image SET categoryid = 403 WHERE imageid = 1790;
+UPDATE site_image SET categoryid = 393 WHERE imageid = 1791;
+UPDATE site_image SET categoryid = 395 WHERE imageid = 1792;
+UPDATE site_image SET categoryid = 394 WHERE imageid = 1793;
+UPDATE site_image SET categoryid = 402 WHERE imageid = 1794;
+UPDATE site_image SET categoryid = 401 WHERE imageid = 1795;
+UPDATE site_image SET categoryid = 400 WHERE imageid = 1796;
+UPDATE site_image SET categoryid = 399 WHERE imageid = 1797;
+UPDATE site_image SET categoryid = 398 WHERE imageid = 1798;
+UPDATE site_image SET categoryid = 406 WHERE imageid = 1799;
+UPDATE site_image SET categoryid = 397 WHERE imageid = 1800;
+UPDATE site_image SET categoryid = 396 WHERE imageid = 1801;
+UPDATE site_image SET categoryid = 403 WHERE imageid = 2292;
+UPDATE site_image SET categoryid =  WHERE imageid = 2301;
+
 -- Images dans une seule catégorie
 DROP TABLE IF EXISTS temp;
 CREATE TABLE temp
@@ -66,6 +126,7 @@ CREATE TABLE temp
 UPDATE site_image AS i
 INNER JOIN temp AS t
 	ON i.imageid = t.imageid
+	AND i.categoryid = 0;
 SET i.categoryid = t.categoryid;
 
 DROP TABLE IF EXISTS temp;
@@ -89,3 +150,8 @@ INNER JOIN site_category AS c
 	ON c.categoryid = ic.categoryid
 	AND c.category_parentid = 26
 SET i.categoryid = c.categoryid;
+
+-- Liens manquants
+INSERT IGNORE INTO site_image_category (imageid, categoryid)
+	SELECT imageid, categoryid
+	FROM site_image;
