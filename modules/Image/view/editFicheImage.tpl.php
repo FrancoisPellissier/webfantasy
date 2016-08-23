2e429b1e6194f15f9234b36bfef0fa9c530877e0
FrancoisPellissier <f_pellissier@hotmail.com>
2016-08-23 22:26:05 +0200

    Wording

--- modules/Image/view/editFicheImage.tpl.php
+++ modules/Image/view/editFicheImage.tpl.php
@@ -1,5 +1,5 @@
 <?php
 echo "\n\t".$form->start("post");
 echo $form->HTMLradio('pictureid', $images, ' | ');
-echo "\n\t".$form->HTMLsubmit('Ajouter');
+echo "\n\t".$form->HTMLsubmit('Associer');
 echo "\n\t".$form->end();
