60b095650780ac2d0cb335530c48cafeae1a83a3
FrancoisPellissier <f_pellissier@hotmail.com>
2016-08-25 08:23:04 +0200

    Add permanent redirection. Fix #80

--- library/HTTPResponse.class.php
+++ library/HTTPResponse.class.php
@@ -7,4 +7,10 @@ class HTTPResponse {
         header("Location: ".($interne ? WWW_ROOT : '').$url);
         exit;
     }
+
+    public function permanentRedirect($url, $interne = true) {
+        header('Status: 301 Moved Permanently', false, 301);
+        header("Location: ".($interne ? WWW_ROOT : '').$url);
+        exit;
+    }
 }
