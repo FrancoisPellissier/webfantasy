5dc5357dc5fea218e8bb82b1ff52a123339719a5
FrancoisPellissier <f_pellissier@hotmail.com>
2016-09-15 13:35:42 +0200

    Style: category. Fix #96

--- modules/Category/view/showCategory.tpl.php
+++ modules/Category/view/showCategory.tpl.php
@@ -1,3 +1,4 @@
+<div class="section" id="section-category">
 <?php
 echo '<h1>'.$category->infos['titre'].'</h1>';
 
@@ -7,16 +8,22 @@ if($user->checkRight()) {
 
 // On affiche les catégories enfants si elles existent
 if(!empty($category->infos['children'])) {
+    echo '<p>';
     foreach($category->infos['children'] AS $cat) {
 
-        echo '<p><a href="'.$model->getSlug().$cat->getSlug().'">'.$cat->infos['titre'].'</a></p>';
+        echo '<a href="'.$model->getSlug().$cat->getSlug().'">'.$cat->infos['titre'].'</a>';
     }
+    echo '<p>';
 }
 // Sinon on affiche les image
-else {
+else if(!empty($category->infos['images'])) {
+    echo '<p>';
     foreach($category->infos['images'] AS $image) {
         $image->migrate();
 
-        echo '<p><a href="'.$model->getSlug().$category->getSlug().'/image/'.$image->infos['imageid'].'"><img src="'.$image->infos['folder'].'/thumbnail/'.$image->infos['filename'].'" title="'.$image->infos['titre'].'" /></a></p>';
+        echo '<a href="'.$model->getSlug().$category->getSlug().'/image/'.$image->infos['imageid'].'"><img src="'.$image->infos['folder'].'/thumbnail/'.$image->infos['filename'].'" title="'.$image->infos['titre'].'" /></a>';
     }
+    echo '<p>';
 }
+?>
+</div>
