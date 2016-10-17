0c2cfe098b2231464d9ed5ddc86fb1ea6b2ed737
FrancoisPellissier <f_pellissier@hotmail.com>
2016-10-17 22:13:30 +0200

    Style: fiche livre. Fix #94

--- modules/Livre/view/showLivre.tpl.php
+++ modules/Livre/view/showLivre.tpl.php
@@ -1,20 +1,35 @@
-<?php
-echo "\n\t".'<h1>'.$model->infos['titre'].'</h1>';
+<div class="section" id="section-livre-fiche">
+    <?php
+    echo "\n\t".'<h1>'.$model->infos['titre'].'</h1>';
+    ?>
+    <table>
+        <tr>
+            <td class="fiche-left">
+        <?php
+            echo "\n\t".'<img src="'.$model->infos['image']->getUrl('small').'" />';
+        ?>
+            </td>
+            <td class="fiche-right">
+        <?php
+            echo "\n\t".'<p><strong>Titre original :</strong> '.$model->infos['titre_vo'].'</p>';
+            echo "\n\t".'<p><strong>Titre français :</strong> '.$model->infos['titre_vf'].'</p>';
 
-echo "\n\t".'<p><img src="'.$model->infos['image']->getUrl('small').'" /></p>';
-
-echo "\n\t".'<p><strong>Titre original :</strong> '.$model->infos['titre_vo'].'</p>';
-echo "\n\t".'<p><strong>Titre français :</strong> '.$model->infos['titre_vf'].'</p>';
-
-// Infos de cycle si fait partie d'un cycle
+            // Infos de cycle si fait partie d'un cycle
 
 
-echo "\n\t".'<p><strong>Date de sortie originale :</strong> '.$model->infos['date_vo'].'</p>';
-echo "\n\t".'<p><strong>Date de sortie française :</strong> '.$model->infos['date_vf'].'</p>';
+            echo "\n\t".'<p><strong>Date de sortie originale :</strong> '.$model->infos['date_vo'].'</p>';
+            echo "\n\t".'<p><strong>Date de sortie française :</strong> '.$model->infos['date_vf'].'</p>';
 
-echo "\n\t".'<p><strong>Présentation :</strong><br/>';
-echo parse_message($model->infos['description'], 0);
-
-if($user->checkRight()) {
-    echo "\n\t".'<p>Administration : <a href="'.$model->getSlug().'/edit">Modifier</a> - <a href="'.$model->getSlug().'/edit/image">Choisir image</a></p>';
-}
+            echo "\n\t".'<p><strong>Présentation :</strong><br/>';
+            echo parse_message($model->infos['description'], 0);
+        ?>
+            </td>
+        </tr>
+    </table>
+    <?php
+    
+    if($user->checkRight()) {
+        echo "\n\t".'<p>Administration : <a href="'.$model->getSlug().'/edit">Modifier</a> - <a href="'.$model->getSlug().'/edit/image">Choisir image</a></p>';
+    }
+    ?>
+</div>
